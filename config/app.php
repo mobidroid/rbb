055dc2a60137ac063ef6afeae65788785a0345f9
JansK <kmjanssen1@gmail.com>
2017-12-05 21:28:47 -0500

    Edited default env values for config/app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'RadioBlahBlah'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://phplaravel-132621-382147.cloudwaysapps.com/'),
 
     /*
     |--------------------------------------------------------------------------
